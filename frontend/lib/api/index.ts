d6e519701dc7bbc2cf0576b289aef4a3358b66eb
Christyan Simbolon <sambungdata@gmail.com>
2026-05-11 16:56:32 +0700

    feat-frontend: add lib index

---
+++ frontend/lib/api/index.ts
@@ -0,0 +1,5 @@
+export { default as apiClient } from "./client";
+export { authApi }       from "./auth";
+export { wargaApi }      from "./warga";
+export { tagihanApi }    from "./tagihan";
+export { komunikasiApi } from "./komunikasi";

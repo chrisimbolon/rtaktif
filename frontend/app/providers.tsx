d7329c637ea85b32f71d8bf929f29d0f1c5b2cd5
Christyan Simbolon <sambungdata@gmail.com>
2026-05-19 21:25:37 +0700

    feat-frontend: update provider

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,8 +1,9 @@
+// app/providers.tsx
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "sonner";
 import { useState } from "react";
+import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({
